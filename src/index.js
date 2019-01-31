2fd2fc7c3db2e14e90293a5b075ad1a9bc2273dc
Alonso Ahmetaj <onsomac@Alonsos-MacBook-Pro.local>
2019-01-31 17:12:04 -0500

    update func comp to class comp

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => {
-      console.log(position);
-    },
-    err => {
-      console.log(err);
-    }
-  );
-  return <div> Another thing</div>;
-};
+class App extends React.Component {
+  //initialize state with cunstructor function
+  constructor(props) {
+    super(props);
+
+    //initialize state as an object with property lat and value null for now.
+    this.state = { lat: null, errMessage: "" };
+
+    //move any function out of Render method so it doesn't get called repeatedly every time Render is called
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        //update state with setState method once the current position is returned from the callback
+        this.setState({ lat: position.coords.latitude });
+        // console.log(position);
+      },
+      err => {
+        //update errMessage state once there is an error and call render to display the message to the user
+        this.setState({ errMessage: err.message });
+        console.log(err);
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errMessage}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
