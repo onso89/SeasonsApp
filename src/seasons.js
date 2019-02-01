e66bc70fd165687278d766458c9a4b048efb7c49
Alonso Ahmetaj <onsomac@Alonsos-MacBook-Pro.local>
2019-02-01 04:04:06 -0500

    refactor the display contents and ternary ops

--- src/seasons.js
+++ src/seasons.js
@@ -1,19 +1,34 @@
+import "./seasons.css";
 import React from "react";
+
+const seasonConfig = {
+  winter: {
+    text: "Is so cold",
+    iconName: "snowflake"
+  },
+  summer: {
+    text: "Is so hot",
+    iconName: "sun"
+  }
+};
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat > 0 ? "summer" : "winter";
+    return lat > 0 ? seasonConfig.summer : seasonConfig.winter;
   } else {
-    return lat > 0 ? "winter" : "summer";
+    return lat > 0 ? seasonConfig.winter : seasonConfig.summer;
   }
 };
 
 const SeasonDisplay = props => {
-  const season = getSeason(props.lat, new Date().getMonth());
-  const temp = season === "winter" ? "Is so cold" : "Is so hot";
+  const { text, iconName } = getSeason(props.lat, new Date().getMonth());
+  //   const text = season === "winter" ? "Is so cold" : "Is so hot";
+  //   const iconName = season === "winter" ? "snowflake" : "sun";
   return (
-    <div>
-      <h1>{temp}</h1>
+    <div className={`season-display ${iconName}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
   );
 };
