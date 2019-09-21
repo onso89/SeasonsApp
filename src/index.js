19c9997d6dbf3ef981bb7dbb39b021228d961645
Alonso Ahmetaj <onsomac@Alonsos-MacBook-Pro.local>
2019-09-21 01:50:04 -0400

    fix some commits

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class App extends React.Component {
       position => {
         //update state with setState method once the current position is returned from the callback
         this.setState({ lat: position.coords.latitude });
-        // console.log(position); => check if position is returned
+        // console.log(position);
       },
       err => {
         //update errMessage state once there is an error and call render to display the message to the user
